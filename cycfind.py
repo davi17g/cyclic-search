d78ed7819b268def4b00e904040c156e65513520
David Gerchikov <davi17g@gmail.com>
2017-03-04 19:53:00 +0200

    Added doc-strings and comments

--- cycfind.py
+++ cycfind.py
@@ -1,19 +1,30 @@
 
 def binary_search(lis, query):
+    """
+    Simple binary search implementation.
+    :param lis: This parameter must be a list of tuples, as following [(key,val),...].
+    :param query:
+    :return: Will be a String type value, if there is no data in list that satisfies the query,
+    empty string will be returned.
+    """
     high = len(lis)-1
     low = 0
     result = ""
+    # if...elif cover some corner cases
     if lis[low][0] == 0:
         result = lis[low][1]
     elif lis[high][0] == query:
         result = lis[high][1]
     else:
+        # main statements of binary search
         while high >= low:
-            mid = (high + low) // 2
+            mid = (high + low) // 2  # middle index of the list
             if lis[mid][0] == query:
                 result = lis[mid][1]
+                # query is satisfied there is no need to continue
                 break
             else:
+                # decision which part of list to choose
                 if lis[mid][0] < query:
                     low = mid + 1
                 else:
@@ -22,31 +33,51 @@ def binary_search(lis, query):
 
 
 def cyclic_search(lis, query):
+    """
+    Binary Search implementation on cyclic list where the head element is moved by arbitrary offset.
+    :param lis: This parameter must be list of tuples as follows: [(key, val)...].
+    :param query:
+    :return: Will be a String type value, if there is no data found NameError Eception will be raised.
+    """
     low = 0
     high = len(lis)-1
     result = ""
+    # if...elif cover some corner cases that may occur.
     if lis[low][0] == query:
         result = lis[low][1]
     elif lis[high][0] == query:
         result = lis[high][1]
     else:
-        while low < high:
+        # Main statements of the algorithm - Process repeats until binary search is invoked
+        # or there is no range left.
+        while low <= high:
             mid = (high + low) // 2
             if lis[mid][0] == query:
+                # if the middle element satisfies the query, immediately terminate the process and return the answer.
                 result = lis[mid][1]
                 break
             elif lis[low][0] < lis[mid][0]:
+                # This case holds when the left hand side of the list is sorted.
                 if query < lis[mid][0]:
+                    # This holds when the query must be in sorted part of the list (a.k.a in LHS part).
+                    # It means that we can invoke binary search to find an answer.
                     result = binary_search(lis[low:mid], query)
                     break
                 else:
+                    # This case holds when LHS is a sorted part of the list but the query is not in range.
+                    # Means that the query is in RHS which is unsorted so the focus is switched to that part.
                     low = mid + 1
             elif lis[mid][0] < lis[high][0]:
+                # This case holds when the right hand side of the list is sorted.
                 if query > lis[mid][0]:
+                    # This holds when the query must be in sorted part of the list (a.k.a in RHS part).
+                    # It means that we can invoke binary search to find the answer.
                     result = binary_search(lis[mid:high+1], query)
                     break
                 else:
+                    # This case holds when RHS is a sorted part of the list but the query is not in range.
+                    # Means that the query is in LHS which is unsorted so the focus is switched to that part.
                     high = mid - 1
     if result == "":
-        raise NameError("The following query: " + query + " Not found!")
+        raise NameError("Query: {0:10s} is not found!".format(query))
     return result
--- present.py
+++ present.py
@@ -1,8 +1,15 @@
+"""
+This an example module that demonstrates the use of the cyclic search on data that is included with
+with the project.
+"""
+
 import sys
 from cycfind import cyclic_search
 OFFSET = 9
 fName = "data.txt"
 data_base = []
+
+# Data is loaded from the text file.
 try:
     with open("data.txt", 'r') as f:
         for line in f:
@@ -14,11 +21,19 @@ except IOError:
 else:
     f.close()
 
+
+# Data is sorted and special list with an arbitrary offset is created.
 data_base.sort(key=lambda x: x[0])
-test_base =data_base[:OFFSET] + data_base[OFFSET:]
+test_base = data_base[:OFFSET] + data_base[OFFSET:]
+# This list is used for querying an existing data.
 names = [name[0] for name in data_base]
+names.append('Something')  # Example for query that is not in data set.
 
 
 for query in names:
-    print("Query: {0:7s} Answer: {1:10s}".format(query, cyclic_search(test_base, query)))
+    try:
+        result = cyclic_search(test_base, query)
+        print("Query: {0:10s} Answer: {1:10s}".format(query, result))
+    except NameError as e:
+        print(e.args)
 
